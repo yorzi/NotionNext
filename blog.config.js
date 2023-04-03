f2232a8e926401097f109b5dd7ee574bad4d3d74
Andy Wang <wangyaodi@gmail.com>
2023-04-03 09:30:17 +0800

    Some updates for aimee blog

--- blog.config.js
+++ blog.config.js
@@ -12,10 +12,10 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '王馆长', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '大龄全栈Web工程师，AI工具狂热追随者。', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://huihuaguan.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'AI绘画, Midjourney, Stable Diffusion, OpenAI, ChatGPT, GPT-3.5, GPT-4', // 网站关键词 英文逗号隔开
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Aimee Wang', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '好好向上，天天学习！', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://wangqichen.com', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '读书,绘画,跑步,小学生生活日常', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'wangyaodi@gmail.com', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
--- themes/hexo/config_hexo.js
+++ themes/hexo/config_hexo.js
@@ -1,6 +1,6 @@
 const CONFIG_HEXO = {
   HOME_BANNER_ENABLE: true,
-  HOME_BANNER_GREETINGS: ['Hi，我是一个大龄程序员', 'Hi，我是一个中年打工人', 'Hi，我是一个独立干饭人', '欢迎来到AI绘画馆🎉'], // 首页大图标语文字
+  HOME_BANNER_GREETINGS: ['Hi，我是一个小学生', 'Hi，我喜欢绘画', 'Hi，我喜欢阅读和跑步', '欢迎来到我的博客🎉'], // 首页大图标语文字
 
   HOME_NAV_BUTTONS: false, // 首页是否显示分类大图标按钮
   HOME_NAV_BACKGROUND_IMG_FIXED: true, // 首页背景图滚动时是否固定，true 则滚动时图片不懂； false则随鼠标滚动
